b82407a0942ab92130013cc761c78dea062ec91d
Alex Goldberg <radio.rat.fm@gmail.com>
2019-12-04 03:27:02 +0200

    Add function to test.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,17 @@
 #include <jni.h>
 #include <string>
+#include "native-lib.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_goldberg_googletest_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */)
 {
-    std::string hello = "Hello from C++";
+    std::string hello = createWelcomingString();
     return env->NewStringUTF(hello.c_str());
+}
+
+std::string createWelcomingString()
+{
+    return "Hello from C++";
 }
